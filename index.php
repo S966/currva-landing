7faa5e6792cbedf8f37552d2f094973b873cc858
rtaibah <taibah@gmail.com>
2014-04-18 22:57:23 +0300

    created a new index.php file. Heroku refuses to accept a push: no Cedar-supported app detected. Adding this file should fix it

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <title>Currrva App: The Multiplayer Soccer Video Game App</title>
+         <link rel="stylesheet" type="text/css" href="bower_components/foundation/css/foundation.css">
+         <link rel="stylesheet" type="text/css" href="assets/stylesheets/style.css">
+    </head>
+    <body>
+            <div class="header row">
+                <div class="large-5 columns">
+                    <h1>Currrva</h1>
+                </div>
+                <div class="large-6 columns">
+                    <ul>
+                        <li><a href="http://facebook.com/getcurrrva"</a><img src="assets/images/icons-facebook.png" alt="facebook" /></a></li>
+                    <li><a href="http://twitter.com/getcurrrva"><img src="assets/images/icons-twitter.png" alt="twitter" /></a></li>
+                </div>
+                <div class="large-offset-1 columns">
+                </div>
+            </div>
+            <div class="main row">
+                <div class="large-6 columns">
+                    <img src="assets/images/phone_02.png" alt="currrva app" />
+                </div>
+                <div class="tagline large-5 columns">
+                    <h1>Victory should never be forgetten</h1>
+                    <h2>Record, visualize, and share all of your soccer video game results.</h2>
+                    <!-- Begin MailChimp Signup Form -->
+                    <div id="mc_embed_signup">
+                        <form action="http://currrva.us8.list-manage.com/subscribe/post?u=03baa6734b1ba1e29373d4fe4&amp;id=b58db5a54d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                                <h2>Provide your email to get into the private beta program :)</h2>
+                                <div class="mc-field-group">
+                                        </label>
+                                            <input placeholder="Provide your email" type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                                        </div>
+                                            <div id="mce-responses" class="clear">
+                                                        <div class="response" id="mce-error-response" style="display:none"></div>
+                                                                <div class="response" id="mce-success-response" style="display:none"></div>
+                                                                    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                                                        <div style="position: absolute; left: -5000px;"><input type="text" name="b_03baa6734b1ba1e29373d4fe4_b58db5a54d" value=""></div>
+                                                                            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                                                                        </form>
+                                                                    </div>
+
+                                                                    <!--End mc_embed_signup-->
+                </div>
+                <div class="large-offset-1 columns">
+                </div>
+        </div>
+        <div class="footer">
+        </div>
+    </body>
+</html>
+
